ad685d3dcdf213c5e26370fe95b35aa0274276e1
Richard Gill <richard@rgill.co.uk>
2024-04-06 13:09:10 +0100

    fix gen

--- packages/react/tsup.config.ts
+++ packages/react/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/components/index.ts", "src/hooks/index.ts"],
   splitting: false,
   sourcemap: true,
   clean: true,
---
+++ tooling/gen/packageTemplate/src/dummy.test.ts.hbs
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("dummy", () => {
+  it("dummy", () => {
+    expect(1).toBe(1);
+  });
+});
