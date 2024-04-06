d65cb4bebda665f8d1aa147377bf58faa1c15ba6
Richard Gill <richard@rgill.co.uk>
2024-04-06 10:38:12 +0100

    fix: exports

---
+++ packages/react/src/components/index.ts
@@ -0,0 +1,1 @@
+export { LLMOutput } from "./LLMOutput";
--- packages/react/src/dummy.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("dummy", () => {
-  it("dummy", () => {
-    expect(1 + 1).toBe(2);
-  });
-});
