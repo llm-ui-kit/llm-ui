bef4dcd7210ac56eee3850918e07308439577362
Richard Gill <richard@rgill.co.uk>
2024-04-07 20:08:37 +0100

    fix typecheck

--- packages/react/src/components/LLMOutput/index.tsx
+++ packages/react/src/components/LLMOutput/index.tsx
@@ -16,7 +16,7 @@ export const LLMOutput: React.FC<LLMOutputProps> = ({
   return (
     <>
       {matches.map(({ component: Component, match }, index) => {
-        return <Component key={index} llmOutput={match.match} />;
+        return <Component key={index} llmOutput={match.output} />;
       })}
     </>
   );
