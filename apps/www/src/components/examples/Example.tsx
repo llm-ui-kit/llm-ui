a4f335c094dff423e4c3a2fadc43675ab2171897
Richard Gill <richard@rgill.co.uk>
2024-04-13 07:18:38 +0100

    Layout ok

--- apps/www/src/components/examples/Example.tsx
+++ apps/www/src/components/examples/Example.tsx
@@ -2,16 +2,16 @@
 import { useStreamFastSmooth } from "@/hooks/useLLMExamples";
 import { cn } from "@/lib/utils";
 import { markdownLookBack } from "@llm-ui/markdown";
-import { useLLMOutput } from "llm-ui/components";
+import { useLLMOutput, type LLMOutputProps } from "llm-ui/components";
 import type { UseStreamWithProbabilitiesOptions } from "llm-ui/hooks";
 import { useState, type ReactNode } from "react";
+import type { SetRequired } from "type-fest";
 import { Loader } from "../ui/Loader";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/Tabs";
 import { H2 } from "../ui/Text";
 import { codeBlockBlock } from "./CodeBlock";
 import { Controls } from "./Controls";
 import { Markdown } from "./Markdown";
-import { example } from "./examples";
 import { throttle } from "./throttle";
 
 const SideBySideContainer: React.FC<{
@@ -20,17 +20,18 @@ const SideBySideContainer: React.FC<{
   children: ReactNode;
 }> = ({ className, header, children }) => {
   return (
-    <div className={cn(className, "overflow-x-hidden flex-1")}>
+    <div className={cn(className, "flex-1 ")}>
       {header}
       {children}
     </div>
   );
 };
 
-const CodeWithBackground: React.FC<{ children: ReactNode }> = ({
+const CodeWithBackground: React.FC<{ children: ReactNode; height: number }> = ({
+  height,
   children,
 }) => (
-  <div className="flex flex-col rounded-lg bg-background p-6 min-h-[500px]">
+  <div style={{ height }} className="rounded-lg bg-background text-left p-6">
     {children}
   </div>
 );
@@ -39,63 +40,82 @@ type OutputTabsProps = {
   output: string;
   llmUi?: ReactNode;
   className?: string;
+  height: number;
 };
 
+type Tab = "markdown" | "raw" | "llm-ui";
 const OutputTabs: React.FC<
   OutputTabsProps & {
-    isMobile?: boolean;
+    tabs?: Tab[];
   }
-> = ({ className, output, llmUi, isMobile = false }) => {
+> = ({
+  className,
+  height,
+  output,
+  llmUi,
+  tabs = ["llm-ui", "markdown", "raw"],
+}) => {
+  const showMarkdown = tabs.includes("markdown");
+  const showRaw = tabs.includes("raw");
+  const showLlmUi = tabs.includes("llm-ui");
+  const defaultValue = tabs[0];
   return (
-    <Tabs defaultValue={isMobile ? "llm-ui" : "markdown"} className={className}>
-      {isMobile && llmUi && <TabsContent value="llm-ui">{llmUi}</TabsContent>}
-      <TabsContent value="markdown">
-        <CodeWithBackground>
-          <Markdown isComplete={false} llmOutput={output} />
-        </CodeWithBackground>
-      </TabsContent>
-      <TabsContent value="raw">
-        <CodeWithBackground>
-          <pre className="overflow-x-auto">{output}</pre>
-        </CodeWithBackground>
-      </TabsContent>
+    <Tabs defaultValue={defaultValue} className={className}>
+      {showLlmUi && llmUi && (
+        <TabsContent value="llm-ui">
+          <div style={{ height }}>{llmUi}</div>
+        </TabsContent>
+      )}
+      {showMarkdown && (
+        <TabsContent value="markdown">
+          <CodeWithBackground height={height}>
+            <Markdown isComplete={false} llmOutput={output} />
+          </CodeWithBackground>
+        </TabsContent>
+      )}
+      {showRaw && (
+        <TabsContent value="raw">
+          <CodeWithBackground height={height}>
+            <pre className="overflow-x-auto">{output}</pre>
+          </CodeWithBackground>
+        </TabsContent>
+      )}
       <div className="flex flex-row items-center mt-2 justify-center md:justify-start">
         <TabsList>
-          {isMobile && (
+          {showLlmUi && llmUi && (
             <TabsTrigger value="llm-ui" className="md:hidden">
               llm-ui
             </TabsTrigger>
           )}
-          <TabsTrigger value="markdown">Markdown</TabsTrigger>
-          <TabsTrigger value="raw">Raw</TabsTrigger>
+          {showMarkdown && <TabsTrigger value="markdown">Markdown</TabsTrigger>}
+          {showRaw && <TabsTrigger value="raw">Raw</TabsTrigger>}
         </TabsList>
       </div>
     </Tabs>
   );
 };
 
-export const Example = ({
-  delayMultiplier: delayMultiplierProp,
+type ExampleProps = {
+  outputHeight: number;
+  tabs?: Tab[];
+} & UseExampleProps;
+
+const LLMUI = ({
+  isStreamFinished,
+  height,
   ...props
-}: Partial<UseStreamWithProbabilitiesOptions>) => {
-  const [delayMultiplier, setDelayMultiplier] = useState(delayMultiplierProp);
-  const { output, isStreamFinished, loopIndex } = useStreamFastSmooth(example, {
-    loop: true,
-    autoStart: true,
-    loopDelayMs: 3000,
-    ...props,
-    delayMultiplier,
-  });
+}: SetRequired<Partial<LLMOutputProps>, "isStreamFinished" | "llmOutput"> & {
+  height: number;
+}) => {
   const { blockMatches, visibleText } = useLLMOutput({
     blocks: [codeBlockBlock],
-    isStreamFinished,
     fallbackBlock: {
       component: Markdown,
       lookBack: markdownLookBack,
     },
-    llmOutput: output,
     throttle,
-    loopIndex,
+    isStreamFinished,
+    ...props,
   });
   const blocks = blockMatches.map((blockMatch, index) => {
     const Component = blockMatch.block.component;
@@ -107,8 +127,8 @@ export const Example = ({
       />
     );
   });
-  const llmUi = (
-    <CodeWithBackground>
+  return (
+    <CodeWithBackground height={height}>
       {visibleText.length === 0 ? (
         <div className="flex flex-1 justify-center items-center">
           <Loader />
@@ -118,36 +138,115 @@ export const Example = ({
       )}
     </CodeWithBackground>
   );
+};
+type UseExampleProps = {
+  example: string;
+  options?: Partial<UseStreamWithProbabilitiesOptions>;
+};
+const useExample = ({ example, options = {} }: UseExampleProps) => {
+  const [delayMultiplier, setDelayMultiplier] = useState(
+    options.delayMultiplier,
+  );
+  const result = useStreamFastSmooth(example, {
+    loop: false,
+    autoStart: true,
+    loopDelayMs: 3000,
+    ...options,
+    delayMultiplier,
+  });
+  return { ...result, setDelayMultiplier };
+};
+
+export const ExampleTabs = ({
+  example,
+  outputHeight,
+  tabs,
+  options,
+}: ExampleProps) => {
+  const { output, isStreamFinished, loopIndex, setDelayMultiplier } =
+    useExample({ example, options });
+  const llmUi = (
+    <LLMUI
+      isStreamFinished={isStreamFinished}
+      llmOutput={output}
+      loopIndex={loopIndex}
+      height={outputHeight}
+    />
+  );
   return (
-    <div>
-      <div className="flex flex-row gap-8 h-[640px]">
+    <div className="grid grid-cols-1">
+      <OutputTabs
+        className="hidden md:block"
+        output={output}
+        llmUi={llmUi}
+        tabs={tabs}
+        height={outputHeight}
+      />
+      <Controls className="md:-mt-12" onDelayMultiplier={setDelayMultiplier} />
+    </div>
+  );
+};
+
+export const ExampleSideBySide = ({
+  className,
+  showHeaders = false,
+  tabs = ["markdown", "raw"],
+  outputHeight,
+  ...props
+}: ExampleProps & { className?: string; showHeaders?: boolean }) => {
+  const { output, isStreamFinished, loopIndex, setDelayMultiplier } =
+    useExample(props);
+  const llmUi = (
+    <LLMUI
+      isStreamFinished={isStreamFinished}
+      llmOutput={output}
+      loopIndex={loopIndex}
+      height={outputHeight}
+    />
+  );
+  return (
+    <div className={className}>
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
         <SideBySideContainer
           header={
-            <H2 className="mb-8 text-muted-foreground text-center">
-              LLM Output
-            </H2>
+            showHeaders && (
+              <H2 className="mb-8 text-muted-foreground text-center">
+                LLM Output
+              </H2>
+            )
           }
         >
-          <OutputTabs className="hidden md:block" output={output} />
           <OutputTabs
-            isMobile
+            className="hidden md:block"
+            output={output}
+            tabs={tabs}
+            height={outputHeight}
+          />
+          <OutputTabs
             className="md:hidden"
             output={output}
             llmUi={llmUi}
+            tabs={["llm-ui", ...tabs]}
+            height={outputHeight}
           />
         </SideBySideContainer>
         <SideBySideContainer
           className="hidden md:block"
           header={
-            <H2 className="mb-8 text-center">
-              <span className="text-gradient_indigo-purple">llm-ui</span> ✨
-            </H2>
+            showHeaders && (
+              <H2 className="mb-8 text-center">
+                <span className="text-gradient_indigo-purple">llm-ui</span> ✨
+              </H2>
+            )
           }
         >
           {llmUi}
         </SideBySideContainer>
       </div>
-      <Controls className="md:-mt-12" onDelayMultiplier={setDelayMultiplier} />
+      <Controls
+        className={cn(tabs.length > 1 && "md:-mt-4")}
+        onDelayMultiplier={setDelayMultiplier}
+      />
     </div>
   );
 };
--- apps/www/src/components/examples/examples.ts
+++ apps/www/src/components/examples/examples.ts
@@ -1,9 +1,22 @@
-export const example = `### [Docs](https://llm-ui.com/docs)
+export const ctaExample = `### [Docs](https://llm-ui.com/docs)
 
 **Install**:
 \`\`\`bash
-pnpm add llm-ui @llm-ui/markdown @llm-ui/code-block
+pnpm add llm-ui @llm-ui/markdown @llm-ui/code-block more more more how do you like it more more how do you like it
 \`\`\`
 
 - *italic*, **bold**, ~strikethrough~,
+`;
+
+export const codeBlockExample = `
+\`\`\`python
+def hello_world():
+    print("Hello, World!")
+\`\`\`
+
+\`\`\`bash
+echo "Hello, World!"
+\`\`\`
+
+
 `;
