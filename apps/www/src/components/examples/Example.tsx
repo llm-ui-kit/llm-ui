968c215417bd34d7ce08a11e3efdde3ea0058a6f
Richard Gill <richardgill@users.noreply.github.com>
2024-04-14 09:37:20 +0100

    Docs: Example auto loop bugfixes (#38)

--- apps/www/src/components/examples/Example.tsx
+++ apps/www/src/components/examples/Example.tsx
@@ -47,6 +47,7 @@ type OutputTabsProps = {
   className?: string;
   tabs?: Tab[];
   tabIndex: number;
+  isVisible: boolean;
 };
 
 const OutputTabs: React.FC<OutputTabsProps> = ({
@@ -55,6 +56,7 @@ const OutputTabs: React.FC<OutputTabsProps> = ({
   tabs = ["llm-ui", "markdown", "raw"],
   className,
   tabIndex,
+  isVisible,
 }) => {
   return (
     <OutputBackground className={className}>
@@ -63,11 +65,18 @@ const OutputTabs: React.FC<OutputTabsProps> = ({
         return (
           <>
             {tab === "markdown" && isActive && (
-              <Markdown isComplete={false} llmOutput={output} />
+              <Markdown
+                className={cn(!isVisible && "invisible")}
+                isComplete={false}
+                llmOutput={output}
+              />
             )}
             {tab === "raw" && isActive && (
               <pre
-                className="not-shiki raw-example "
+                className={cn(
+                  "not-shiki raw-example",
+                  !isVisible && "invisible",
+                )}
                 style={{ backgroundColor: "inherit" }}
               >
                 {output}
@@ -108,7 +117,11 @@ const LLMUI = ({
   isPlaying: boolean;
   hideFirstLoop?: boolean;
 }) => {
-  const { blockMatches, visibleText } = useLLMOutput({
+  const {
+    blockMatches,
+    visibleText,
+    loopIndex: outputLoopIndex,
+  } = useLLMOutput({
     blocks: [codeBlockBlock],
     fallbackBlock: {
       component: Markdown,
@@ -134,10 +147,10 @@ const LLMUI = ({
       <div
         className={cn(
           "flex flex-1 flex-col",
-          hideFirstLoop && loopIndex === 0 && "invisible",
+          hideFirstLoop && outputLoopIndex === 0 && "invisible",
         )}
       >
-        {visibleText.length === 0 && isPlaying && loopIndex !== 0 ? (
+        {visibleText.length === 0 && isPlaying && outputLoopIndex !== 0 ? (
           <div className="flex flex-1 justify-center items-center">
             <Loader />
           </div>
@@ -205,6 +218,7 @@ export const ExampleTabs: React.FC<ExampleProps> = ({
     <div className={cn("grid grid-cols-1", className)}>
       <NeverShrinkContainer className="flex flex-1">
         <OutputTabs
+          isVisible={!hideFirstLoop || loopIndex !== 0}
           className={cn(backgroundClassName, "flex flex-1 rounded-t-lg")}
           output={output}
           llmUi={llmUi}
@@ -273,6 +287,7 @@ export const ExampleSideBySide: React.FC<ExampleSideBySideProps> = ({
       hideFirstLoop={hideFirstLoop}
     />
   );
+  const isVisible = !hideFirstLoop || loopIndex !== 0;
   return (
     <div className={className}>
       <NeverShrinkContainer className="grid md:grid-cols-2 grid-cols-1 ">
@@ -286,6 +301,7 @@ export const ExampleSideBySide: React.FC<ExampleSideBySideProps> = ({
           }
         >
           <OutputTabs
+            isVisible={isVisible}
             className={cn(
               backgroundClassName,
               "hidden md:flex border-r-2 border-background-200 rounded-tl-lg overflow-clip",
@@ -295,6 +311,7 @@ export const ExampleSideBySide: React.FC<ExampleSideBySideProps> = ({
             tabIndex={desktopTabIndex}
           />
           <OutputTabs
+            isVisible={isVisible}
             className={cn(
               backgroundClassName,
               "md:hidden rounded-t-lg overflow-clip",
