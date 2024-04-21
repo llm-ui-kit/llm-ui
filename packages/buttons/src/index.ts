8d44f22f81d16bcdc23728ac2245e38116034bc4
Richard Gill <richard@rgill.co.uk>
2024-04-21 11:24:16 +0100

    wip

--- packages/buttons/src/index.ts
+++ packages/buttons/src/index.ts
@@ -2,3 +2,4 @@ export { buttonsBlock } from "./block";
 export { buttonsLookBack } from "./lookBack";
 export { buttonsCompleteMatcher, buttonsPartialMatcher } from "./matchers";
 export { parseCompleteButtons } from "./parse";
+export { buttonPrompt } from "./prompt";
