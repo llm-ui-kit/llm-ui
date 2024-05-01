07db2432b9285d3078f8dd88e6ae65b8446fd7d5
Richard Gill <richard@rgill.co.uk>
2024-05-01 16:45:00 +0000

    fix examples

--- apps/www/src/components/examples/examples.ts
+++ apps/www/src/components/examples/examples.ts
@@ -12,17 +12,6 @@ It renders **bold**, *italic*, and ~strikethrough~ without showing any markdown 
 
 Here's a link to the [docs](https://llm-ui.com/docs).
 
-### Matches your display's frame rate
-This text is streaming tokens which are 3 characters long, but llm-ui smooths this out by rendering characters at the native frame rate of your display.
-
-### Removes pauses
-
-It can also smooth our pauses in the LLM's response
-
-Like this one: in 3...2...1`;
-
-const ctaAfterPause = `without users noticing
-
 ### Custom components
 
 Add your own custom components to your LLM output.
@@ -41,7 +30,17 @@ This works by telling the LLM to reply with buttons like this:
 \`\`\`
 
 ^^^ It also has code blocks with syntax highlighting for over 100 languages with [Shiki](https://shiki.style/).
-`;
+
+### Matches your display's frame rate
+This text is streaming tokens which are 3 characters long, but llm-ui smooths this out by rendering characters at the native frame rate of your display.
+
+### Removes pauses
+
+It can also smooth out pauses in the LLM's response
+
+Like this one: in 3...2...1`;
+
+const ctaAfterPause = `without users noticing.`;
 export const ctaExample: TokenWithDelay[] = [
   ...stringToTokenArray(ctaBeforePause, defaultExampleProbs),
   { token: " ", delayMs: 1500 },
