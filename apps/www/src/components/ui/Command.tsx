0413f6f12b6a5472ea48d5dbf5f4e22ce357d064
Richard Gill <richardgill@users.noreply.github.com>
2024-05-01 18:39:45 +0100

    Docs styling fixes (#148)

--- apps/www/src/components/ui/Command.tsx
+++ apps/www/src/components/ui/Command.tsx
@@ -33,7 +33,11 @@ const CommandDialog = ({
 }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0">
+      <DialogContent
+        className={
+          "overflow p-0 top-[20%] translate-y-0 data-[state=closed]:slide-out-to-top-[18%] data-[state=open]:slide-in-from-top-[18%]"
+        }
+      >
         <Command
           {...commandProps}
           className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5"
