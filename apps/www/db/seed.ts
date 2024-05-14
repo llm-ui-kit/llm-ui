7e92ae0e4ca946506ae4b080e2a099ba5994d4f0
DanPGill <dangillsocialmediaapp@gmail.com>
2024-05-14 15:07:56 +0100

    Adds ChatRequest table to astro db and seeds basic example

--- apps/www/db/seed.ts
+++ apps/www/db/seed.ts
@@ -1,4 +1,4 @@
-import { db, WaitingList } from "astro:db";
+import { ChatRequest, WaitingList, db } from "astro:db";
 
 // https://astro.build/db/seed
 export default async () => {
@@ -8,4 +8,10 @@ export default async () => {
       { email: "johndoe@example.com" },
       { email: "janedoe@example.com" },
     ]);
+  await db.insert(ChatRequest).values([
+    {
+      systemContent: "You are a helpful assistant.",
+      userContent: "Show me a demo of all the markdown header sizes",
+    },
+  ]);
 };
