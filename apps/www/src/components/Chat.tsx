e4476330f46e7e47d728ef185c9e370abea9a371
DanPGill <danpgill@gmail.com>
2024-05-20 20:32:36 +0100

    Chat page (#201)
    Co-authored-by: DanPGill <dangillsocialmediaapp@gmail.com>

---
+++ apps/www/src/components/Chat.tsx
@@ -0,0 +1,82 @@
+import { Message } from "@/components/demo/message/Message";
+import { Button } from "@/components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { Textarea } from "@/components/ui/Textarea";
+
+import * as React from "react";
+
+export const Chat = () => {
+  const [userContent, setUserContent] = React.useState<string>("");
+  const [currentApiKey, setCurrentApiKey] = React.useState<string>();
+  const [output, setOutput] = React.useState<string>("");
+  const [isStreamFinished, setIsStreamFinished] =
+    React.useState<boolean>(false);
+
+  const handleUpdateUserContent = (
+    e: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const newUserContent = e.target.value;
+    setUserContent(newUserContent);
+  };
+
+  const handleUpdateApiKey = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newApiKey = e.target.value;
+    setCurrentApiKey(newApiKey);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const body = { userContent, systemContent: "You are a helpful assistant." };
+    const response = await fetch("/api/chatRequest", {
+      method: "POST",
+      headers: {
+        Accept: "application.json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    const data = await response.json();
+    const { id } = data;
+    setOutput("");
+    if (id && currentApiKey) {
+      const eventSource = new EventSource(
+        `/api/chat/openai?id=${id}&apiKey=${currentApiKey}`,
+      );
+
+      eventSource.addEventListener("error", () => {
+        eventSource.close();
+      });
+
+      eventSource.addEventListener("token", (e) => {
+        // avoid newlines getting messed up
+        const token = e.data.replaceAll("$NEWLINE$", "\n");
+        setOutput((prevResponse) => `${prevResponse}${token}`);
+      });
+
+      eventSource.addEventListener("finished", (e) => {
+        console.log("finished", e);
+        eventSource.close();
+        setIsStreamFinished(true);
+      });
+
+      () => eventSource.close();
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="space-y-8">
+        <Textarea
+          placeholder="What would you like to know?"
+          value={userContent}
+          onChange={handleUpdateUserContent}
+          className="mb-2"
+        />
+        <label>Enter Your API Key</label>
+        <Input onChange={handleUpdateApiKey} />
+        <Message message={output} isStreamFinished={isStreamFinished} />
+        <Button type="submit">Start</Button>
+      </form>
+    </div>
+  );
+};
---
+++ apps/www/src/pages/api/chat/openai.ts
@@ -0,0 +1,91 @@
+export const prerender = false;
+
+import type { APIRoute } from "astro";
+import { ChatRequest, db, eq } from "astro:db";
+import OpenAI from "openai";
+
+const NEWLINE = "$NEWLINE$";
+
+interface OpenAiErrorType {
+  error: {
+    message?: string;
+  };
+  status?: number;
+}
+
+export const GET: APIRoute = async ({ request }) => {
+  const { url } = await request;
+  const params = new URL(url).searchParams;
+  const id = Number(params.get("id"));
+  const apiKey = params.get("apiKey") || "";
+
+  if (!id || !apiKey) {
+    return new Response(JSON.stringify({ message: "Missing id or apiKey" }), {
+      status: 400,
+    });
+  }
+
+  const chat = await db
+    .select()
+    .from(ChatRequest)
+    .where(eq(ChatRequest.id, id));
+  const { userContent, systemContent } = chat[0];
+  if (!chat || !chat.length) {
+    return new Response(
+      JSON.stringify({
+        message: "Not found",
+      }),
+      { status: 400 },
+    );
+  }
+
+  try {
+    const openai = new OpenAI({
+      apiKey,
+    });
+    let responseStream = new TransformStream();
+    const writer = responseStream.writable.getWriter();
+    const encoder = new TextEncoder();
+
+    const completion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: systemContent },
+        {
+          role: "user",
+          content: userContent,
+        },
+      ],
+      stream: true,
+    });
+
+    (async () => {
+      for await (const chunk of completion) {
+        const content = chunk.choices[0].delta.content;
+        if (content !== undefined && content !== null) {
+          // avoid newlines getting messed up
+          const contentWithNewlines = content.replace(/\n/g, NEWLINE);
+          await writer.write(
+            encoder.encode(`event: token\ndata: ${contentWithNewlines}\n\n`),
+          );
+        }
+      }
+
+      await writer.write(encoder.encode(`event: finished\ndata: true\n\n`));
+      await writer.close();
+    })();
+    return new Response(responseStream.readable, {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache, no-transform",
+      },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({
+        message: (error as OpenAiErrorType).error.message,
+      }),
+      { status: (error as OpenAiErrorType).status },
+    );
+  }
+};
