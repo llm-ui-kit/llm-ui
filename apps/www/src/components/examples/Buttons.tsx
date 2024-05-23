df60a573b9a6a85ed0c047d612ca340f5876a82c
Richard Gill <richard@rgill.co.uk>
2024-05-23 17:43:41 +0100

    working

--- apps/www/src/components/examples/Buttons.tsx
+++ apps/www/src/components/examples/Buttons.tsx
@@ -1,27 +1,31 @@
 import { fireConfetti } from "@/animations/confetti";
 import { multipleStars } from "@/animations/stars";
-import {
-  buttonsLookBack,
-  findCompleteButtons,
-  findPartialButtons,
-  parseCompleteButtons,
-} from "@llm-ui/buttons";
+import { customBlock, parseJson5 } from "@llm-ui/custom";
 import type { LLMOutputBlock, LLMOutputComponent } from "@llm-ui/react";
+import z from "zod";
 import { Button } from "../ui/Button";
 
-type OnClick = (buttonText: string) => void;
+type OnClick = (buttonText: string | undefined) => void;
+
+const schema = z.object({
+  t: z.literal("b"),
+  b: z.array(z.object({ v: z.string() })),
+});
+
+const partialSchema = schema.deepPartial();
 
 const buttonsComponent = (onClick: OnClick) => {
   const ButtonsComponent: LLMOutputComponent = ({ blockMatch }) => {
-    const buttons = parseCompleteButtons(blockMatch.output);
+    console.log("blockMatch.output", blockMatch.output);
+    const buttons = partialSchema.parse(parseJson5(blockMatch.output));
     if (!buttons) {
       return undefined;
     }
     return (
       <div className="flex flex-row my-4 gap-2">
-        {buttons.map((button, index) => (
-          <Button key={index} onClick={() => onClick(button)}>
-            {button}
+        {buttons?.b?.map((button, index) => (
+          <Button key={index} onClick={() => onClick(button?.v)}>
+            {button?.v}
           </Button>
         ))}
       </div>
@@ -41,8 +45,6 @@ export const starsAndConfetti = (buttonText: string) => {
 export const buttonsBlock = (
   onClick: OnClick = starsAndConfetti,
 ): LLMOutputBlock => ({
-  findCompleteMatch: findCompleteButtons(),
-  findPartialMatch: findPartialButtons(),
-  lookBack: buttonsLookBack(),
+  ...customBlock("b"),
   component: buttonsComponent(onClick),
 });
--- packages/custom/src/index.ts
+++ packages/custom/src/index.ts
@@ -2,3 +2,4 @@ export { customBlock } from "./block";
 export { customBlockLookBack } from "./lookback";
 export { findCompleteCustomBlock, findPartialCustomBlock } from "./matchers";
 export type { CustomBlockOptions } from "./options";
+export { parseJson5 } from "./parseJson5";
