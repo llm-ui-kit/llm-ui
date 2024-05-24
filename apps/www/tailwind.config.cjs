bd63248b954502627f138b2cf5d259109d87336e
DanPGill <danpgill@gmail.com>
2024-05-24 10:22:14 +0100

    Removes unused styles

--- apps/www/tailwind.config.cjs
+++ apps/www/tailwind.config.cjs
@@ -60,10 +60,6 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        warning: {
-          DEFAULT: "hsl(var(--warning))",
-          foreground: "hsl(var(--warning-foreground))",
-        },
       },
       borderRadius: {
         lg: "var(--radius)",
