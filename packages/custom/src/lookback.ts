f7056e4c0aff72e6c91561be08c8e41d73a7e591
Richard Gill <richard@rgill.co.uk>
2024-05-27 08:55:55 +0100

    defaultVisible to allow invisible and visible

--- packages/custom/src/lookback.ts
+++ packages/custom/src/lookback.ts
@@ -1,6 +1,6 @@
 import { LookBackFunction } from "@llm-ui/react";
 import { setJsonPath } from "./jsonPathSet";
-import { traverseLeafNodesWithIgnores } from "./jsonPathTraverse";
+import { isAllowed, isIgnored, traverseLeafNodes } from "./jsonPathTraverse";
 import { CustomBlockOptions, getOptions } from "./options";
 import { parseJson5 } from "./parseJson5";
 import { removeStartEndChars } from "./shared";
@@ -10,17 +10,21 @@ export const customBlockLookBack = (
   userOptions?: Partial<CustomBlockOptions>,
 ): LookBackFunction => {
   const options = getOptions(userOptions);
-  const { invisibleKeyPaths, typeKey } = options;
-  return ({ output, visibleTextLengthTarget }) => {
+  const { typeKey, defaultVisible, visibleKeyPaths, invisibleKeyPaths } =
+    options;
+  return ({ output, visibleTextLengthTarget, isComplete }) => {
     const object = parseJson5(removeStartEndChars(output, options));
     if (!object || object[typeKey] !== type) {
       return { output: "", visibleText: "" };
     }
-    const invisiblePaths = [`$.${typeKey}`, ...invisibleKeyPaths];
     let remainingChars = visibleTextLengthTarget;
 
     let visibleText = "";
-    traverseLeafNodesWithIgnores(object, invisiblePaths, (value, path) => {
+    const shouldTraverse = defaultVisible
+      ? isIgnored([`$.${typeKey}`, ...invisibleKeyPaths])
+      : isAllowed(visibleKeyPaths);
+
+    traverseLeafNodes(object, shouldTraverse, (value, path) => {
       const valueString = `${value}`;
       const chars = Math.min(remainingChars, valueString.length);
       const valueVisible = valueString.slice(0, chars);
@@ -29,9 +33,15 @@ export const customBlockLookBack = (
       remainingChars -= chars;
     });
 
+    const isStreamingKeys = defaultVisible || visibleKeyPaths.length > 0;
+
     return {
       output: JSON.stringify(object, null, 2),
-      visibleText,
+      visibleText: isStreamingKeys
+        ? visibleText
+        : isComplete && visibleTextLengthTarget > 0
+          ? " " // Show a space if the block is complete and there is no visible text
+          : "",
     };
   };
 };
