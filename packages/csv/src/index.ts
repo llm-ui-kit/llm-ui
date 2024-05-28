f919604d5fa21aa8ae96142985300b38777925b3
Richard Gill <richard@rgill.co.uk>
2024-05-28 16:12:00 +0000

    matchers

--- packages/csv/src/index.ts
+++ packages/csv/src/index.ts
@@ -2,5 +2,5 @@
 // export { csvBlockExample } from "./csvBlockExample";
 // export { csvBlockPrompt } from "./csvBlockPrompt";
 // export { csvBlockLookBack } from "./lookback";
-// export { findCompleteCsvBlock, findPartialCsvBlock } from "./matchers";
+export { findCompleteCsvBlock, findPartialCsvBlock } from "./matchers";
 export type { CsvBlockOptions } from "./options";
