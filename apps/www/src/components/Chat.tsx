f49a4d0824a7a8b51e027d613fbbcea78e13d8cd
DanPGill <danpgill@gmail.com>
2024-05-30 10:55:01 +0100

    Adds styling for user chat bubbles when in light mode (#247)

--- apps/www/src/components/Chat.tsx
+++ apps/www/src/components/Chat.tsx
@@ -36,7 +36,7 @@ const ChatMessage: React.FC<{
       <div
         className={cn(
           "space-y-2 overflow-x-hidden",
-          role === "user" && "bg-black rounded-lg px-4 py-1",
+          role === "user" && "bg-background rounded-lg px-4 py-1",
         )}
       >
         <MessageComponent
