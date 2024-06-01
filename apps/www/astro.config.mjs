2476a3721216d231808fb1445847c829987f3020
Richard Gill <richard@rgill.co.uk>
2024-06-01 16:42:00 +0000

    www: Local previews

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,5 +1,6 @@
 import db from "@astrojs/db";
 import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
@@ -7,6 +8,14 @@ import vercel from "@astrojs/vercel/serverless";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
+
+let adapter = vercel({
+  maxDuration: 60,
+});
+
+if (process.env.ADAPTER === "node") {
+  adapter = node({ mode: "standalone" });
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -34,7 +43,5 @@ export default defineConfig({
     simpleStackForm(),
   ],
   output: "hybrid",
-  adapter: vercel({
-    maxDuration: 60,
-  }),
+  adapter,
 });
